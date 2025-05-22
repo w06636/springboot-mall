6525e7d47bb126c4b133e24787132f581580bfd8
w06636 <willie06636@gmail.com>
2025-05-22 15:12:09 +0800

    first commit

---
+++ src/test/java/com/demo/springbootmall/SpringbootmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
