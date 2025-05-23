955da33134db7b7a9d1dd0a0a5cbf39065e76f80
w06636 <willie06636@gmail.com>
2025-05-23 14:07:15 +0800

    update

--- src/main/java/com/demo/springbootmall/controller/ProductController.java
+++ src/main/java/com/demo/springbootmall/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.demo.springbootmall.controller;
 
 import com.demo.springbootmall.model.Product;
 import com.demo.springbootmall.service.ProductService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+	
+	private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     @Autowired
     private ProductService productService;
@@ -22,6 +27,7 @@ public class ProductController {
         if (product != null) {
             return ResponseEntity.status(HttpStatus.OK).body(product);
         }
+        log.info("查無商品");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
